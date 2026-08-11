e737fcd1bbd71f4e24608d470ad64a6e29b3dd02
vanichols <virginia.nichols@gmail.com>
2026-08-11 11:06:02 +0200

    add basic mgmt data

---
+++ data-raw/sexy1/mgmt/01_proc_sexy1_mgmt.R
@@ -0,0 +1,32 @@
+# created           aug 2026
+# purpose:          get easy access to general mgmt things
+# last updated:
+
+library(tidyverse)
+library(readxl)
+
+rm(list = ls())
+
+
+
+# 1. 24/25 season ---------------------------------------------------------
+
+d <- read_excel("data-raw/sexy1/mgmt/sexy1_mgmt-created-manually-from-fieldlog.xlsx",
+           sheet = "sexy1x1",
+           skip = 5)
+
+d1 <-
+  d |>
+  fill(loc_name, sea_name) |>
+  janitor::clean_names() |>
+  mutate(date_ymd = ymd(date_dmy))
+
+# write it ----------------------------------------------------------------
+
+sexy1_mgmt <-  d1
+
+usethis::use_data(sexy1_mgmt, overwrite = TRUE)
+
+sexy1_mgmt %>%
+  write_csv("inst/extdata/sexy1_mgmt.csv")
+
